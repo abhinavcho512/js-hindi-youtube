049c28d7f87519a794c6438b90b75a1d950e6a1f
Abhinav Choudhary <121492908+abhinavcho512@users.noreply.github.com>
2024-12-01 10:44:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =144553
+let accountEmail="abhinav@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"   // not good thing :)
+let accountState;
+
+accountEmail="acac@gmail.com"
+accountPassword="181217"
+accountCity="bengaluru"
+
+//accountId=2    //not allowed
+console.log(accountId);
+
+/*
+{
+scope problem -> var so dont use  now (due to block scope and functional scope)
+}
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"   // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs,not console browser so it will give no output
+
+console.log(3
+    +3);   // code readability should be high
+
+    let name="hitesh"
+    let age=18
+    let isLoggedIn=false
+
+    // number => 2^53
+    // bigint
+    // string =>""
+    // boolean =>true/false
+    // null=> standalone value   //let state=null(empty)
+    // undefined=> letState;  // no value assign
+    //symbol => used in react / used for uniqueness
+
+    //object
+
+console.log(typeof null)
+
+
