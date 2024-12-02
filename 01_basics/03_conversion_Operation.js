a9d2fde2171061396de96d9bb2724dafce17cdf1
Abhinav Choudhary <121492908+abhinavcho512@users.noreply.github.com>
2024-12-02 09:20:26 +0000

    conversion

--- 01_basics/03_conversion_Operation.js
+++ 01_basics/03_conversion_Operation.js
@@ -9,16 +9,47 @@ console.log(valueInNumber);
 // "33abc"=>NaN
 // true=>1 ; false=>0
 
-let isLoggedIn=""
+// let isLoggedIn=""
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //  1=>true ;  0=>false
 //  ""=> false
 // "hitesh"=>true
 
-let someNumber=33
+// let someNumber=33
 
-let stringNumber=String(someNumber)
-console.log(typeof stringNumber)
+// let stringNumber=String(someNumber)
+// console.log(typeof stringNumber)
+
+// **************** Operations   ************//
+
+// let val=3
+// let negval=-val
+// console.log(negval);
+
+let str1="hello"
+let str2=" abhinav"
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);   // convert ans in string
+// console.log("1"+2+2);
+// console.log(1+"2");
+// console.log(1+2+"2");   // first add and then string(add)
+
+// console.log((3+4)*(5%3));   // dont write messy code
+
+// console.log(+true);   // ans=1
+// console.log(true+);    //ans=error
+// console.log(+"");    // + is used for conversion into numbers
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
