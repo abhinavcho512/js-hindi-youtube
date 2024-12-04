d4fa69ace07c9cb1b2ef6e9aa3e31519f3b2ba1f
Abhinav Choudhary <121492908+abhinavcho512@users.noreply.github.com>
2024-12-04 10:38:17 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,49 @@
+// ++++++++++++   Numbers   ++++++++++
+
+// const score =400
+// console.log(score);
+
+
+// const balance=new Number(100)  // only number will come
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));  // mainly to show client 100.00
+
+// const otherNumber=123.8966  //op->124
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-In'));
+
+// +++++++++++    Maths   +++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));      // op->4
+// console.log(Math.round(4.3));   // op->4  (**mostly used)
+// console.log(Math.ceil(4.2));   // op->5 (uper kr dega)
+// console.log(Math.floor(4.8));   // op->4  (niche kr dega)
+// console.log(Math.sqrt(5));
+//console.log(Math.min(5,4,8,2,9));
+
+console.log(Math.random());               // op->0.3345663  (0-1)
+console.log((Math.random()*10)+1);         // op->4.6457464664  (0.1+1=1.1) (1-9)
+console.log(Math.floor(Math.random()*10)+1);   // op->8  (1-9)
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);    // op-> exactly between 10 and 20  [V.IMP]
+
+
+
+
+
+
+
+
+
+
+
+
